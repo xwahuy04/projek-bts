96f2767c27785e922b146cab3ac635839a169596
AkbarDAfandi <akbardafandi@gmail.com>
2024-04-17 18:08:00 +0700

    fixed login logic

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -11,36 +11,29 @@ class Login extends CI_Controller{
         $this->load->view('v_login');
     }
 
-    public function aksi_login(){
-        $nis = $this->input->post('nis');
-        $where = array(
-            'nis' => $nis
-        );
-        try {
-            $query = $this->m_login->cek_login("nis",$where);
-            if ($query !== false) {
-                $cek = $query->num_rows();
-                if($cek > 0){
-                    $data_session = array(
-                        'nis' => $nis,
-                        'status' => "login"
-                    );
-                    $this->session->set_userdata($data_session);
-                    redirect(base_url("v_admin"));
-                } else {
-                    echo "nis salah !";
-                }
-            } else {
-                // Handle the case where the query failed
-                echo "Terjadi kesalahan saat mencoba untuk masuk. Silakan coba lagi";
-            }
-        } catch (Exception $e) {
-            // Log the error message for debugging
-            log_message('error', $e->getMessage());
-            // Display a user-friendly error message
-            echo "Terjadi kesalahan saat mencoba untuk masuk. Silakan coba lagi.";
-        }
-    }
+	public function aksi_login(){
+		$nis = $this->input->post('nis');
+		$where = array('nis' => $nis);
+		try {
+			$query = $this->m_login->cek_login('user',$where);
+			if ($query !== false) {
+				$cek = $query->num_rows();
+				if($cek > 0){
+					$data_session = array('nis' => $nis, 'status' => "login");
+					$this->session->set_userdata($data_session);
+					redirect(base_url("admin"));
+				} else {
+					echo "nis salah !";
+				}
+			} else {
+				echo "Query failed";
+			}
+		} catch (Exception $e) {
+			log_message('error', $e->getMessage());
+			echo "Terjadi kesalahan saat mencoba untuk masuk. Silakan coba lagi.";
+		}
+	}
+	
     
 
     public function do_login() {
@@ -48,7 +41,7 @@ class Login extends CI_Controller{
         
         // Lakukan validasi login sesuai kebutuhan, misalnya dengan model pengguna
         $this->load->model('User_model');
-        $user = $this->User_model->login($nis,);
+        $user = $this->User_model->login($nis);
     
         if ($user) {
             // Jika login berhasil, atur sesi dan redirect ke halaman v_admin
